56d7cfafcc8b573c35c18abf9a6bd74649508592
laidu <laidu823@gmail.com>
2018-04-02 22:44:23 +0800

    init

---
+++ src/main/java/org/xp/coder/XpApplication.java
@@ -0,0 +1,12 @@
+package org.xp.coder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XpApplication.class, args);
+	}
+}
---
+++ src/test/java/org/xp/coder/XpApplicationTests.java
@@ -0,0 +1,16 @@
+package org.xp.coder;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
